c1ce0a1fa605f9da2bd67c29f7474da15f0e8979
Leo Kuboschek <leo@kuboschek.me>
2024-12-11 07:42:46 +0100

    Day 11: Too slow, didn't run

---
+++ 2024/11/1.py
@@ -0,0 +1,39 @@
+from functools import reduce
+
+def load_from_string(s: str) -> list[list[str]]:
+    return [list(line) for line in s.strip().split('\n')]
+
+def main():
+    #print(check('sample-input', solve))
+    print(check('real-input', solve))
+
+def check(input_file, func):
+    with open(input_file) as f:
+        input_str = f.read()
+    return func(input_str)
+
+def iterate(stones):
+    def reducer(new_stones, stone):
+        if stone == 0:
+            new_stones.append(1)
+        elif len(str(stone)) % 2 == 0:
+            left, right = int(str(stone)[:len(str(stone))//2]), int(str(stone)[len(str(stone))//2:])
+            new_stones.extend([left, right])
+        else:
+            new_stones.append(stone * 2024)
+        return new_stones
+
+    return reduce(reducer, stones, [])
+
+def solve(input_str: str):
+    stones = list(map(int, input_str.split(' ')))
+
+    for i in range(75):
+        print(i)
+        stones = iterate(stones)
+
+    return len(stones)
+
+
+if __name__ == '__main__':
+    main()
