fc2fe3f74cea0afc6802eabebce8e60d723263b9
Leo Kuboschek <leo@kuboschek.me>
2025-12-04 17:14:02 +0100

    3.2

--- 2025/3/2.py
+++ 2025/3/2.py
@@ -1,7 +1,27 @@
 import itertools
 
 def evaluate_expression(input_str: str):
-    return sum(map(lambda string: max(map(lambda combo: int("".join(combo)), itertools.combinations(string, 12))), input_str.splitlines()))
+    sum = 0
+    
+    for line in input_str.splitlines():
+        sum += int(get_joltage(line))
+
+    return sum
+
+def get_joltage(string, length=12, current=""):
+    if length > len(string):
+        return None
+    if length==0:
+        return current
+    if length==1:
+        return current + max(string)
+
+    for c in "987654321":
+        if c in string:
+            index = string.index(c)
+            substr = string[index+1:]
+            if (result := get_joltage(substr, length-1, current+c)) is not None:
+                return result
 
 
 def check(input_file, func):
